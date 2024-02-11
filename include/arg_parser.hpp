cf5e715f227ca3deea521b21b209148ffa3972d9
Aviral Singh <aviralsingh@protonmail.com>
2024-02-11 11:08:59 +0530

    Formatting fix

--- include/arg_parser.hpp
+++ include/arg_parser.hpp
@@ -1,26 +1,33 @@
-#include <vector>
-#include <string>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
-class ArgParser {
-private:
-  std::vector<std::string> arguments;
+#pragma once
+class ArgParser
+{
+  private:
+    std::vector<std::string> arguments;
 
-  std::vector<std::string> parse(int argc, char** argv) {
-    if (argc < 1) {
-      throw std::runtime_error("invalid number of arguments");
+    std::vector<std::string> parse(int argc, char **argv)
+    {
+        if (argc < 1)
+        {
+            throw std::runtime_error("invalid number of arguments");
+        }
+        std::vector<std::string> args(argc - 1);
+        for (int i = 1; i < argc; i++)
+            args[i - 1] = argv[i];
+        return args;
     }
-    std::vector<std::string> args(argc - 1);
-    for (int i = 1; i < argc; i++) args[i - 1] = argv[i];
-    return args;
-  }
 
-public:
-  ArgParser(int argc, char** argv) {
-    this->arguments = this->parse(argc, argv);
-  };
+  public:
+    ArgParser(int argc, char **argv)
+    {
+        this->arguments = this->parse(argc, argv);
+    };
 
-  std::vector<std::string> get_arguments() {
-    return this->arguments;
-  }
+    std::vector<std::string> get_arguments()
+    {
+        return this->arguments;
+    }
 };
